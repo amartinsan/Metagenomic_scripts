c55c16245fa2a29ac642ed15d4460ad4da514dd6
Adrian Mtz <4dri4nms@gmail.com>
2023-03-22 17:52:25 -0600

    Update Ordinations_Phyloseq.R

--- Amplicon_scripts/Ordinations_Phyloseq.R
+++ Amplicon_scripts/Ordinations_Phyloseq.R
@@ -1,30 +1,49 @@
-library(yingtools2)
-library(umap)
+
+#Microbiome Ordinations wiht Phyloseq and more
+#!R
+
+#load libraries
 library("phyloseq")
 library("microbiome")
-Muestras=rCLRdata
-ordi_NMDS <- ordinate(Muestras,"NMDS","bray",)
-p=plot_ordination(Muestras, ordi_NMDS, color = "ordennum",shape = "batch")  +  geom_point(size=2) + scale_color_viridis(option = "turbo") + ggtitle("NMDS") + theme_bw()+theme(legend.title = element_blank(), axis.title = element_text(size=16), axis.text = element_text(size = 14))
-ax=ggplotly(p)
-htmlwidgets::saveWidget(as_widget(ax), "NMDS.html")
-#PCoA en vez de NMDS
-ordi_PCoA <- ordinate(Muestras,"PCoA","bray")
-p=plot_ordination(Muestras, ordi_PCoA, color = "ordennum",shape = "batch")  +  geom_point(size=2.5) + scale_color_viridis() + ggtitle("PCoA") + theme_bw() + theme(legend.title = element_blank(), axis.title = element_text(size=16), axis.text = element_text(size = 14))
-ax=ggplotly(p)
-htmlwidgets::saveWidget(as_widget(ax), "PCoA.html")
-#PCA
-ordi_PCA <- ordinate(Muestras,method = "RDA")
-p=plot_ordination(Muestras, ordi_PCA, color = "ordennum",shape = "batch")  +  geom_point(size=2.5) + scale_color_viridis() + ggtitle("PCA") + theme_bw() + theme(legend.title = element_blank(), axis.title = element_text(size=16), axis.text = element_text(size = 14))
-ax=ggplotly(p)
-htmlwidgets::saveWidget(as_widget(ax), "PCA.html")
+library("tidyverse")
+library("yingtools2")
+library("umap")
+library("dada2")
+library("biomformat")
+library("vegan")
 
-#UMAP
-s <- get.samp(Muestras)
-otu <- get.otu(Muestras,as.matrix=TRUE) %>% t()
+
+##############
+muestra=$PHYLOSEQfile
+#basic ordinations (NMDS,PCoA,PCA,etc.), normal bray distance for example
+
+ordi_NMDS <- ordinate(physeq = muestra,method = "NMDS","bray") 
+ordi_PCoA <- ordinate(physeq = muestra,method = "PCoA","bray")
+ordi_PCA <- ordinate(physeq = muestra,method = "RDA","bray")
+
+####Plot with ggplot2##### you can sapply o for cycle to optimize
+
+NMDSPlot = plot_ordination(muestra, ordi_NMDS) + geom_point(size=3) + theme_bw() 
+    + theme(legend.title = element_blank())
+
+PCoAplot = plot_ordination(muestra, ordi_PCoA) + geom_point(size=3) + theme_bw() 
+    + theme(legend.title = element_blank())
+
+PCAplot = plot_ordination(muestra, ordi_NMDS) + geom_point(size=3) + theme_bw() 
+    + theme(legend.title = element_blank())
+
+###UMAP , a little trickier ####
+#get samples and OTUs well ASVs 
+s <- get.samp(muestra)
+otu <- get.otu(muestra,as.matrix=TRUE) %>% t()
 umap <- umap(otu)
 layou=data.frame(umap$layout) %>% rownames_to_column("sample") %>% rename(UMAP1=X1,UMAP2=X2) %>% left_join(s,by="sample")
+UMAPplot = ggplot(layou,aes(x=UMAP1,y=UMAP2,color=ordennum)) + 
+    geom_point(size=2,aes(shape=layou$batch)) + 
+    theme_bw() + 
+    theme(legend.title = element_blank(),)
 
-p=ggplot(layou,aes(x=UMAP1,y=UMAP2,color=ordennum)) + geom_point(size=2,aes(shape=layou$batch)) + scale_color_viridis(option = "cividis") + theme_bw() + ggtitle("UMAP") + theme_bw() + theme(legend.title = element_blank(), axis.title = element_text(size=16), axis.text = element_text(size = 14))
 
-ax=ggplotly(p)
-htmlwidgets::saveWidget(as_widget(ax), "UMAP.html")
+
+
+
