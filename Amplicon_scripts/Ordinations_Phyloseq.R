e18eb097712a477d6ab2f2ca5b11f1931eed5bc4
Adrian Mtz <4dri4nms@gmail.com>
2023-03-22 18:33:44 -0600

    Update Ordinations_Phyloseq.R

--- Amplicon_scripts/Ordinations_Phyloseq.R
+++ Amplicon_scripts/Ordinations_Phyloseq.R
@@ -43,7 +43,45 @@ UMAPplot = ggplot(layou,aes(x=UMAP1,y=UMAP2,color=ordennum)) +
     theme_bw() + 
     theme(legend.title = element_blank(),)
 
+#####Make a graph "interactive with plotly" #######
+#Example
 
+Muestras=rCLRdata
+ordi_NMDS <- ordinate(Muestras,"NMDS","bray",)
+p=plot_ordination(Muestras, ordi_NMDS, color = "ordennum",shape = "batch")  +  
+    geom_point(size=2) + scale_color_viridis(option = "turbo") + 
+    ggtitle("NMDS") + theme_bw() + 
+    theme(legend.title = element_blank(), 
+        axis.title = element_text(size=16), 
+        axis.text = element_text(size = 14))
+
+#save plot as gglotly and to save as an .html file, maybe aplly this in shiny       
+ax=ggplotly(p)
+htmlwidgets::saveWidget(as_widget(ax), "NMDS.html")
+
+#Example on how to cycle a core microbiome graph of a a bunch of phyloseq objects in a vector
+
+
+#The assing and paste is to save the graphs in a variable with the name of the vector variables
+
+clientes <- c("URIEL","CLAUDIA","YESSICA","REGINA","PABLO","MONTSE","microbiofit")
+
+for (i in clientes) {
+  Muestra <- get(i)
+  mtype <- as.character(Muestra@sam_data$orden[order(paste(Muestra@sam_data$nombre,Muestra@sam_data$orden))]) 
+  div_alpha <- estimate_richness(Muestra, measures = c("Shannon", "Simpson"))
+  # extract Shannon and Simpson indices from the alpha diversity table
+  div_alpha <- as.data.frame(div_alpha[c("Shannon", "Simpson")])
+  # add a column for the sample IDs
+  div_alpha$Sample <- rownames(div_alpha)
+  # convert data to long format
+  div_alpha_long <- tidyr::gather(div_alpha, "measure", "value", -Sample)
+  # create barplot using ggplot2
+  assign(paste("alpha_", i, sep = ""), ggplot(div_alpha_long, aes(x = Sample, y = value, fill = measure)) +
+           geom_bar(stat = "identity", position = "dodge") +
+           scale_fill_manual(values = c("skyblue", "lightgreen")) +
+           labs(x = "Sample", y = "Alpha diversity") + theme_bw() + scale_x_discrete(limits=mtype))
+}
 
 
 
